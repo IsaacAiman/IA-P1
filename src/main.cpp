06d70fcc562464ad71fca2afd44355166af3cb4b
alu0100890839 <alu0100890839@ull.edu.es>
2016-10-20 21:18:21 +0100

    Organizando ficheros

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,10 @@
 #include <stdio.h>
-#include <allegro5/allegro.h>
 #include <cstdlib>
 #include <iostream>
-#include "allegro5/allegro_native_dialog.h"
-#include "allegro5/allegro_image.h"
 #include <ctime>
+#include "../header/aux_func.hpp"
 
 const float FPS = 60;
-
-
-ALLEGRO_BITMAP *load_bitmap_at_size(const char *filename, int w, int h)
-{
-    ALLEGRO_BITMAP *resized_bmp, *loaded_bmp, *prev_target;
-
-    resized_bmp = al_create_bitmap(w, h);
-    if (!resized_bmp) return NULL;
-
-    loaded_bmp = al_load_bitmap(filename);
-    if (!loaded_bmp)
-    {
-        al_destroy_bitmap(resized_bmp);
-        return NULL;
-    }
-
-    prev_target = al_get_target_bitmap();
-    al_set_target_bitmap(resized_bmp);
-
-    al_draw_scaled_bitmap(loaded_bmp,
-        0, 0,                                // source origin
-        al_get_bitmap_width(loaded_bmp),     // source width
-        al_get_bitmap_height(loaded_bmp),    // source height
-        0, 0,                                // target origin
-        w, h,                                // target dimensions
-        0                                    // flags
-    );
-
-  // 5. restore the previous target and clean up
-    al_set_target_bitmap(prev_target);
-    al_destroy_bitmap(loaded_bmp);
-
-    return resized_bmp;
-}
-
-
 
 int main(int argc, char **argv)
 {
